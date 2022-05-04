451606e66d55a66c488c7c66bc5958cd0d7c678b
Drago Tomasic <drag0540@stud.kea.dk>
2022-05-04 18:20:37 +0200

    Intro finish

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import "./Intro.css";
+import Instagram from "../../img/instagram.png";
+import LinkedIn from "../../img/linkedin.png";
+import Github from "../../img/github.png";
+
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import Drago from "../../img/drago1.png";
+import thumbup from "../../img/thumbup.png";
+import Crown from "../../img/crown.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import FloatingDiv from "../FloatingDiv/FloatingDiv";
 
 const Intro = () => {
   return (
@@ -10,14 +21,42 @@ const Intro = () => {
           <span>Drago</span>
           <span>
             secretary-general of the Communist Party of the Soviet Union
-            (1922-53) and premier of the Soviet state (1941-53), who for a
-            quarter of a century dictatorially ruled the Soviet Union and
-            transformed it into a major world power.
+            (1922-53) and premier of the Soviet state (1941-53)
           </span>
         </div>
         <button className="button i-button">Hire me</button>
+        <div className="i-icons">
+          <a href="www.index.hr">
+            <img src={Instagram} alt="" />
+          </a>
+          <img src={LinkedIn} alt="" />
+          <img src={Github} alt="" />
+        </div>
       </div>
-      <div className="i-right"></div>
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={Drago} alt="" />
+        <img src={glassesimoji} alt="" />
+        <div style={{ top: "-4%", left: "68%" }}>
+          <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
+        </div>
+        <div style={{ top: "18rem", left: "0rem" }}>
+          <FloatingDiv image={thumbup} txt1="Best player" txt2="Web game" />
+        </div>
+        {/*blur divs*/}
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
