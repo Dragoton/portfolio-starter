145936372717151173378da8f7a252a8034ce11d
Drago Tomasic <drag0540@stud.kea.dk>
2022-05-05 13:39:13 +0200

    experienceFinish

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Experience.css";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="achievement">
+        <div className="circle">8+</div>
+        <span>years</span>
+        <span>experience</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">10+</div>
+        <span>projects</span>
+        <span>playes</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">20</div>
+        <span>years</span>
+        <span>experience</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
