a86ac53e10aff1411bed7f7ae3ebad1f916a5e2c
Drago Tomasic <drag0540@stud.kea.dk>
2022-05-06 11:12:36 +0200

    testimonial slut

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -5,6 +5,9 @@ import profilePic1 from "../../img/profile1.jpg";
 import profilePic2 from "../../img/profile2.jpg";
 import profilePic3 from "../../img/profile3.jpg";
 import profilePic4 from "../../img/profile4.jpg";
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css";
 
 const Testimonial = () => {
   const clients = [
@@ -42,12 +45,18 @@ const Testimonial = () => {
         <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
       </div>
       {/*Swipper */}
-      <Swiper>
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
         {clients.map((client, index) => {
           return (
             <SwiperSlide key={index}>
-              <img src={client.img} alt="" />
-              <span>{client.review}</span>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+              </div>
             </SwiperSlide>
           );
         })}
